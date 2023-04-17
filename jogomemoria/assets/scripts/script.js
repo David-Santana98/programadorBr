4c0a3ddaf867475ae7bed471d04b0ed0c30b1a43
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-04-17 19:59:02 -0300

    montando os cards com js

--- jogomemoria/assets/scripts/script.js
+++ jogomemoria/assets/scripts/script.js
@@ -1,42 +1,79 @@
 const FRONT = "card_front";
 const BACK = "card_back";
 
-let techs= ['bootstrap',
-'CSS',
-'electron',
-'firebase',
-'html',
-'javascript',
-'jquery',
-'mongo',
-'node',
-'react'];
+let techs = [
+  "bootstrap",
+  "CSS",
+  "electron",
+  "firebase",
+  "html",
+  "javascript",
+  "jquery",
+  "mongo",
+  "node",
+  "react",
+];
 
-createCardsFromTechs(techs);
-function createCardsFromTechs(techs) {
-    
-    let cards = [];
+let cards = null;
 
-    for(let tech of techs) {
-        cards.push(createPairFromTech(tech));
-    }
+startGame();
 
-    return cards.flatMap(pair => pair);
+function startGame() {
+  cards = createCardsFromTechs(techs);
+  shuffleCards(cards);
+  initializeCards(cards);
+
 }
 
 
+function initializeCards(cards) {
+  let gameBoard = document.getElementById("gameBoard")
+
+    cards.forEach(card => {
+
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id;
+    })
+}
+
+
+function shuffleCards(cards) {
+  let currentIndex = cards.length;
+  let randomIndex = 0;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex); //pegando o último elemento do array
+    currentIndex--;
+
+    [cards[randomIndex], cards[currentIndex]] = [
+      cards[currentIndex],
+      cards[randomIndex],
+    ];
+  }
+}
+
+function createCardsFromTechs(techs) {
+  let cards = [];
+
+ techs.forEach((tech) =>  {
+    cards.push(createPairFromTech(tech));
+  })
+
+  return cards.flatMap((pair) => pair);
+}
+
 function createPairFromTech(tech) {
-    
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }, { id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,}]
+  return [
+    {
+      id: createIdWithTech(tech),
+      icon: tech,
+      flipped: false,
+    },
+    { id: createIdWithTech(tech), icon: tech, flipped: false },
+  ];
 }
 
 // criando o ID
 function createIdWithTech(tech) {
-    return tech + parseInt(Math.random() * 1000);
+  return tech + parseInt(Math.random() * 1000);
 }

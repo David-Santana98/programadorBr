6af1b499ac1231993cc343644c64b844df9eb8ea
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-04-18 16:37:26 -0300

    ajustes

--- sort.js
+++
@@ -1,8 +0,0 @@
-// entendo sobre .sort()
-// podemos receber uma array de números aleatórios onde precisam ser ordenados
-let numeros = [99, 42, 10, 26, 27, 51, 89];
-
-numeros = numeros.sort();
-
-
-console.log(numeros)

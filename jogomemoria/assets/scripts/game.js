9a781190b248ad58bd931e9ccd9785dbb31dc538
David Santana <91860681+David-Santana98@users.noreply.github.com>
2023-04-28 19:48:43 -0300

    método para liberar as cartas

--- jogomemoria/assets/scripts/game.js
+++ jogomemoria/assets/scripts/game.js
@@ -28,6 +28,11 @@ let game = {
     return this.firstCard.icon === this.secondCard.icon;
   },
 
+  clearCards: function() {
+      this.firstCard = null;
+      this.secondCard == null;
+      this.lockMode = false;
+  },
 
   techs: [
     "bootstrap",
